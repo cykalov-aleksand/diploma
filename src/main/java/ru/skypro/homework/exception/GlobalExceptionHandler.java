f313c9fb8eea0fe4125f7977f6bdc702c3ce8aa1
1в <cykalov.aleksand@mail.ru>
2025-07-26 10:29:47 +0300

    добавление глобальных исключений

--- src/main/java/ru/skypro/homework/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/skypro/homework/exception/GlobalExceptionHandler.java
@@ -7,22 +7,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-        @ExceptionHandler(ForbiddenException.class)
+    @ExceptionHandler(ForbiddenException.class)
     public ResponseEntity<String> handleForbiddenException
-    (ForbiddenException e) {
-        // Возвращаем статус 403
+            (ForbiddenException e) {
         return ResponseEntity.status(403).build();
     }
+
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<String> handleNotFoundException
             (NotFoundException e) {
-        // Возвращаем статус 404
         return ResponseEntity.status(404).build();
     }
+
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<String> handleUnauthorizedException
             (UnauthorizedException e) {
-        // Возвращаем статус 401
         return ResponseEntity.status(401).build();
     }
 }
