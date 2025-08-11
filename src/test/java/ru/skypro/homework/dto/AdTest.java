afc211d110691a9a62ef9eeeb6a8cc4939b5ae7c
andrejstrogonov <strogonovandrej@hotmail.com>
2025-08-11 21:38:45 +0300

    Add tests

---
+++ src/test/java/ru/skypro/homework/dto/AdTest.java
@@ -0,0 +1,64 @@
+package ru.skypro.homework.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdTest {
+
+    @Test
+    void getAuthor() {
+    }
+
+    @Test
+    void getImage() {
+    }
+
+    @Test
+    void getPk() {
+    }
+
+    @Test
+    void getTitle() {
+    }
+
+    @Test
+    void getPrice() {
+    }
+
+    @Test
+    void setAuthor() {
+    }
+
+    @Test
+    void setImage() {
+    }
+
+    @Test
+    void setPk() {
+    }
+
+    @Test
+    void setTitle() {
+    }
+
+    @Test
+    void setPrice() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void canEqual() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
