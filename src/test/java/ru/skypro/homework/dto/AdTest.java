45fe22811c51eba9600dd38872e1b3f38384bca5
andrejstrogonov <strogonovandrej@hotmail.com>
2025-08-11 21:40:52 +0300

    Fix tests with delombok

--- src/test/java/ru/skypro/homework/dto/AdTest.java
+++ src/test/java/ru/skypro/homework/dto/AdTest.java
@@ -1,64 +1,55 @@
 package ru.skypro.homework.dto;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-class AdTest {
+public class AdTest {
 
     @Test
-    void getAuthor() {
+    public void testConstructorAndGetters() {
+        Ad ad = new Ad(1, "http://example.com/image.jpg", 100, "Test Title", 500);
+        assertEquals(1, ad.getAuthor());
+        assertEquals("http://example.com/image.jpg", ad.getImage());
+        assertEquals(100, ad.getPk());
+        assertEquals("Test Title", ad.getTitle());
+        assertEquals(500, ad.getPrice());
     }
 
     @Test
-    void getImage() {
+    public void testSetters() {
+        Ad ad = new Ad();
+        ad.setAuthor(2);
+        ad.setImage("http://example.com/newimage.jpg");
+        ad.setPk(200);
+        ad.setTitle("New Title");
+        ad.setPrice(1000);
+        assertEquals(2, ad.getAuthor());
+        assertEquals("http://example.com/newimage.jpg", ad.getImage());
+        assertEquals(200, ad.getPk());
+        assertEquals("New Title", ad.getTitle());
+        assertEquals(1000, ad.getPrice());
     }
 
     @Test
-    void getPk() {
+    public void testEquals() {
+        Ad ad1 = new Ad(1, "http://example.com/image.jpg", 100, "Test Title", 500);
+        Ad ad2 = new Ad(1, "http://example.com/image.jpg", 100, "Test Title", 500);
+        Ad ad3 = new Ad(2, "http://example.com/image2.jpg", 101, "Another Title", 600);
+        assertEquals(ad1, ad2);
+        assertNotEquals(ad1, ad3);
     }
 
     @Test
-    void getTitle() {
+    public void testHashCode() {
+        Ad ad1 = new Ad(1, "http://example.com/image.jpg", 100, "Test Title", 500);
+        Ad ad2 = new Ad(1, "http://example.com/image.jpg", 100, "Test Title", 500);
+        assertEquals(ad1.hashCode(), ad2.hashCode());
     }
 
     @Test
-    void getPrice() {
-    }
-
-    @Test
-    void setAuthor() {
-    }
-
-    @Test
-    void setImage() {
-    }
-
-    @Test
-    void setPk() {
-    }
-
-    @Test
-    void setTitle() {
-    }
-
-    @Test
-    void setPrice() {
-    }
-
-    @Test
-    void testEquals() {
-    }
-
-    @Test
-    void canEqual() {
-    }
-
-    @Test
-    void testHashCode() {
-    }
-
-    @Test
-    void testToString() {
+    public void testToString() {
+        Ad ad = new Ad(1, "http://example.com/image.jpg", 100, "Test Title", 500);
+        String expected = "Ad(author=1, image=http://example.com/image.jpg, pk=100, title=Test Title, price=500)";
+        assertEquals(expected, ad.toString());
     }
 }
